4c1d4ee482b568db4392ac6c219fe55441d4f1c1
Star-Wars-Man <jacob@olsenweb.net>
2025-01-07 09:13:26 -0700

    Grade

--- set1/2.py
+++ set1/2.py
@@ -1,1 +1,11 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter a score: "))
+if num >= 90:
+    print("A")
+elif num >= 80:
+    print("B")
+elif num >= 70:
+    print("C")
+elif num >= 60:
+    print("D")
+else:
+    print("F")
