1109babc7f4f32541655f45ab6a8e233527e33ba
Star-Wars-Man <jacob@olsenweb.net>
2025-01-09 08:25:18 -0700

    create 4

---
+++ set1/4.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+if num >= 1 and num <= 10:
+    print("The number is between 1 and 10")
+elif num >= 11 and num <= 20:
+    print("The number is between 11 and 20")
+else:
+    print("The number is out of range")
