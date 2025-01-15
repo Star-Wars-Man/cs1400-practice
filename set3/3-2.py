a1ba6d97b74d88b4af339da2b6393c5db3000ae5
Star-Wars-Man <jacob@olsenweb.net>
2025-01-15 08:57:30 -0700

    3-2

---
+++ set3/3-2.py
@@ -0,0 +1,18 @@
+customer_type = input("Are you a 'member' or 'guest'? ").lower()
+years = int(input("How many years have you been a member? ")) if customer_type == "member" else 0
+first_time = input("Is this your first visit? (yes/no): ").lower() if customer_type == "guest" else ""
+
+if customer_type == "member" and years >= 5:
+    print("You get a 20% discount.")
+elif customer_type == "member" and years >= 2:
+    print("You get a 10% discount.")
+elif customer_type == "member" and years < 2:  
+    print("You get a 5% discount.")
+
+elif customer_type == "guest" and first_time == "yes":
+    print("You get a 5% discount.")
+
+elif customer_type == "guest" and first_time == "no":
+    print("No discount for repeat guests.")
+else:
+    print("Invalid customer type.")
